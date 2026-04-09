a9e82d7828d4cb0cab45b234b200e7d8a40e324a
Night <jakkrinmes@gmail.com>
2026-04-09 23:04:45 +0700

    fix: resolve unused variable conflict

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import ProtectedRoute from "./components/ProtectedRoute";
+// import ProtectedRoute from "./components/ProtectedRoute";
 import PublicRoute from "./components/PublicRoute";
 
 import NotFound from "./pages/NotFound";
